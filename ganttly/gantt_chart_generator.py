f8b072d85eb92e7392cb50129dfcf6156ae8bfc7
Ciro Lucio Tecce <c.tecce@reply.it>
2024-08-06 15:10:21 +0200

    GanttChartGenerator: input validation

--- ganttly/gantt_chart_generator.py
+++ ganttly/gantt_chart_generator.py
@@ -11,7 +11,25 @@ class GanttChartGenerator:
     def __init__(self, activities: List[ActivityDTO]):
         self.activities = activities
 
+    def _validate_activities(self):
+        """
+        Perofrms the following checks depending on the activity type: 
+            1. TASK: both start_date and end_date are set
+            2. MILESTONE: start_date is set
+            """
+        for activity in self.activities:
+            match activity.activity_type:
+                case ActivityTypeEnum.TASK:
+                    if not activity.start_date or not activity.end_date:
+                        raise ValueError(
+                            f"Activity {activity.activity} is missing start_date or end_date")
+                case ActivityTypeEnum.MILESTONE:
+                    if not activity.start_date:
+                        raise ValueError(
+                            f"Activity {activity.activity} is missing start_date")
+
     def draw_chart(self):
+        self._validate_activities()
         df = pd.DataFrame([{
             'Sub Stream': activity.sub_stream,
             'Activity': f"{activity.activity}-{activity.activity_category}",
---
+++ tests/gantt_chart_generator_test.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from ganttly.dto import ActivityTypeEnum
+from ganttly.gantt_chart_generator import GanttChartGenerator
+from tests.activity_dto_helper import build_activity_dto
+
+
+class TestGanttChartGenerator(TestCase):
+
+    def test_should_fail_for_task_with_no_end_date(self):
+        activity = build_activity_dto()
+        activity.activity_type = ActivityTypeEnum.TASK
+        activity.end_date = None
+        activities = [
+            activity
+        ]
+        gcg = GanttChartGenerator(activities)
+        with self.assertRaises(ValueError):
+            gcg.draw_chart()
+
+    def test_should_fail_for_task_with_no_start_date(self):
+        activity = build_activity_dto()
+        activity.activity_type = ActivityTypeEnum.TASK
+        activity.start_date = None
+        activities = [
+            activity
+        ]
+        gcg = GanttChartGenerator(activities)
+        with self.assertRaises(ValueError):
+            gcg.draw_chart()
+
+    def test_should_fail_for_milestone_with_no_start_date(self):
+        activity = build_activity_dto()
+        activity.activity_type = ActivityTypeEnum.MILESTONE
+        activity.start_date = None
+        activities = [
+            activity
+        ]
+        gcg = GanttChartGenerator(activities)
+        with self.assertRaises(ValueError):
+            gcg.draw_chart()
